9251f55092699e47eae000b4520f38d43267a28b
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2021-02-13 18:44:14 -0600

    reorganization of files

--- colorSelector.m
+++
@@ -1,9 +0,0 @@
-function color = colorSelector(maxFValue, positiveRanges, negativeRanges)
-if maxFValue <= positiveRanges(1) && maxFValue >= negativeRanges(1)
-    color = 'g';
-elseif maxFValue <= positiveRanges(2) && maxFValue >= negativeRanges(2)
-    color = 'y';
-else
-    color = 'r';
-end
-end
